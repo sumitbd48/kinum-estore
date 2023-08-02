780e54252e669c966c88aab1b6fba0e424412103
sumitbd48 <sumit.buddhacharya48@gmail.com>
2023-08-02 06:53:22 +0545

    Created login and register page

---
+++ client/src/pages/login/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+function Login() {
+return (
+<div>Hi this is login</div>
+)
+}
+export default Login
---
+++ client/src/pages/register/index.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+
+const SignupSchema = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  lastName: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+  username: Yup.string()
+    .min(5, "Too Short!")
+    .max(20, "Too Long!")
+    .required("Required"),
+});
+
+export const Register = () => (
+  <div>
+    <h1>Signup</h1>
+    <Formik
+      initialValues={{
+        firstName: "",
+        lastName: "",
+        email: "",
+        username: "",
+      }}
+      validationSchema={SignupSchema}
+      onSubmit={(values) => {
+        // same shape as initial values
+        console.log(values);
+      }}
+    >
+      {({ errors, touched }) => (
+        <Form className="form">
+          <Field placeholder="firstName" name="firstName" />
+          {errors.firstName && touched.firstName ? (
+            <div>{errors.firstName}</div>
+          ) : null}
+          <br></br>
+          <Field placeholder="lastName" name="lastName" />
+          {errors.lastName && touched.lastName ? (
+            <div>{errors.lastName}</div>
+          ) : null}
+          <br></br>
+          <Field placeholder="email" name="email" type="email" />
+          {errors.email && touched.email ? <div>{errors.email}</div> : null}
+          <br></br>
+          <Field placeholder="username" name="username"/>
+          {errors.username && touched.username ? <div>{errors.username}</div> : null}
+          <br></br>
+          <button type="submit">Submit</button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
+
+export default Register;
